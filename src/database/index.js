0783dfcf5609757554d394969f5947bb920fd6f5
Marco Melchiorri <marco.melchiorri@verisure.it>
2024-04-11 17:46:03 +0200

    wip

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,5 @@
-const mysql = require('mysql2/promise');
+const connection = require('./createConnection')
 
-const options = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-}
-
-module.exports = async () => await mysql.createConnection(options);
+module.exports = [
+    connection,
+];
